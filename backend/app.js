460dbc69f19926eb2cbd3c5bc212801aacb71f73
Shivangi4Dubey <111shivangi.dubey@gmail.com>
2025-09-03 00:43:15 +0530

    day2

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,11 @@ const app= express();
 const dotenv=require('dotenv');
 dotenv.config();
 const cors=require('cors');
+const connectToDb=require('./db/db');
 
+
+// Connect to MongoDB
+connectToDb();
 //On production level we will specify domains from which requests are allowed and all others are blocked
 app.use(cors());
 
---
+++ backend/models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const  bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
+
+
+
+const userSchema= new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type:String,
+            required:true,
+            minlength:[3,'First name must be atleast 3 characters long'],
+        },
+        lastname:{
+            type:String,
+            minlength:[3,'Last name must be atleast 3 characters long']
+        }
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength:[5,'Email must be atleast 5 charcters long'],
+
+    },
+    password:{
+        type:String,
+        required:true,
+        //pswd will not be sent during requests from user
+        select:false,
+    },
+    //for live tracking  of driver and caption 
+    socketId:{
+        type:String,
+    }
+});
+
+userSchema.methods.generateAuthToken=function(){
+    const token=jwt.sign({_id:this._id},process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePAssword=async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.statics.hashPassword=async function(password){
+    return await bcrypt.hash(password,10);
+}
+
+const userModel =mongoose.model('user',userSchema);
+module.exports=userModel;
